c5920681df602bde0336a7e4c212f52124808452
ajshopov <alexanderjshopov@gmail.com>
2019-06-03 15:05:41 +1000

    steps 4 and 5

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -2,11 +2,31 @@ import { Template } from 'meteor/templating';
 
 import { Tasks } from "../api/tasks.js";
 
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
     tasks() {
-        return Tasks.find({});
+        return Tasks.find({}, { sort: { createdAt: -1 } });
     },
 });
 
+Template.body.events({
+    'submit .new-task'(event) {
+        // prevent browser submit
+        event.preventDefault();
+
+        //  get value from form
+        const target = event.target;
+        const text = target.text.value;
+
+        //  add to collection
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        // clear form
+        target.text.value = '';
+    },
+});
